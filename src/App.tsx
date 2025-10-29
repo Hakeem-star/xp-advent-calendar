779bb1ecfea7dd7dc15e1d449aa1728f3dae9c0c
Hakeem Ladejobi <Hladejobi@proofpoint.com>
2025-10-29 17:32:30 +0000

    wip
    Signed-off-by: Hakeem Ladejobi <Hladejobi@proofpoint.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,54 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div style={{ color: "red" }}>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>CHRISTMAS</div>
-    </>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "black",
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          border: "1px solid red",
+          width: "fit-content",
+          borderRadius: "1rem",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(5, 1fr)",
+            gridTemplateRows: "repeat(5, 1fr)",
+            padding: "2rem",
+            gap: "2rem",
+          }}
+        >
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div
+              key={index}
+              style={{
+                color: "red",
+                fontSize: "2rem",
+                width: "15vw",
+                height: "15vh",
+                outline: "1px solid red",
+                borderRadius: "1rem",
+              }}
+            >
+              {index + 1}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ color: "red", fontSize: "2rem" }}>CHRISTMAS</div>
+    </div>
   );
 }
 
