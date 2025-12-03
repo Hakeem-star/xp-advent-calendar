06f9a207f0521648bfd11ee5db504270c40c5807
Hakeem Ladejobi <hakeem.ladejobi@tessian.com>
2025-12-03 23:48:53 +0000

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/xp-advent-calendar",
+  base: "/xp-advent-calendar",
   plugins: [
     react({
       babel: {
