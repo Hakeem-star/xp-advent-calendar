11866d351ac36907cacb8fd27b701045b08bc2c6
Hakeem Ladejobi <hakeem.ladejobi@tessian.com>
2025-12-03 23:12:27 +0000

    change to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import DayPage from "./DayPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/xp-advent-calendar" element={<App />} />
         <Route path="/xp-advent-calendar/:dayId" element={<DayPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
